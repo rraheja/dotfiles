fa36e91a49f13d198fbb74c0f66c8c46e18f7609
Rajesh Raheja <rajesh.raheja@gmail.com>
2011-02-28 14:09:35 -0800

    Initial commit

---
+++ scripts/tc-mount.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Mount TrueCrypt Volume
+REM Rajesh Raheja
+
+REM Configuration Settings
+
+set TC_BIN=D:\portableapps\truecrypt\truecrypt.exe
+set TC_VOL=D:\Documents\Dropbox\Vault\docvault.tc
+
+start %TC_BIN% /q background /e /c y /l r /m rm /v %TC_VOL%
