f68918c4733e755cae4a39481d6a994c5b3f99fd
Rajesh Raheja <rajesh.raheja@gmail.com>
2012-12-27 12:31:50 -0800

    Added instructions on use for Crashplan

--- rraheja-nas-mount.bat
+++ rraheja-nas-mount.bat
@@ -1,3 +1,6 @@
+#Schedule this using Windows Task Scheduler to run at login using the SYSTEM account
+#Change "welcome" to the password for the NAS
+
 echo %date% %time% : "%cd%\mount.bat" >> C:\cp-nas-mount.log 2>&1 
 
 net use /delete M: 2>&1
