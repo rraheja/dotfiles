630502c5f6f1f160535999e3ebebcb551f913344
Rajesh Raheja <rajesh.raheja@gmail.com>
2013-01-12 21:42:29 -0800

    Removed scripts

--- rraheja-nas-mount.bat
+++
@@ -1,7 +0,0 @@
-REM Schedule this using Windows Task Scheduler to run at login using the SYSTEM account
-REM Change "welcome" to the password for the NAS
-
-echo %date% %time% : "%cd%\mount.bat" >> C:\cp-nas-mount.log 2>&1 
-
-net use /delete M: 2>&1
-net use M: \\rraheja-nb\Media /USER:rraheja-nb\guest welcome >> C:\cp-nas-mount.log 2>&1
