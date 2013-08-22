f797e586bb14e9c61a15c9ef578e4e9047d3e840
Rajesh Raheja <rajesh.raheja@gmail.com>
2013-08-21 23:28:09 -0700

    changed backup approach from rsync to tar

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Backup script - backup location can be passed in as first argument.
-# Rajesh Raheja 08/09/2013
+# Rajesh Raheja 08/21/2013
 
 HOSTNAME=`hostname`
 BACKUPDIR=${1:-rraheja@rraheja-nas.local:/mnt/Share/Backups/"$HOSTNAME"}
@@ -8,18 +8,13 @@ BACKUPDIR=${1:-rraheja@rraheja-nas.local:/mnt/Share/Backups/"$HOSTNAME"}
 exec > >(tee /tmp/backup.log)
 exec 2>&1
 
-echo Backing up "$HOSTNAME" homes
-rsync -avhRHX --delete --stats /home "$BACKUPDIR" --exclude='Downloads' --exclude='*[Cc]ache' --exclude='nepomuk*' --exclude='.wine' --exclude='.local' --exclude='Trash' --exclude='lost+found'
-
 echo Exporting APT keys
 apt-key exportall > /etc/apt/repo.keys
 
 echo Archiving /etc configuration files
-tar -cvzf "$BACKUPDIR"/etc.tgz -T backup.list --exclude-caches-all --ignore-failed-read
+tar -cvzf "$BACKUPDIR"/backup.tgz -T backup.list --exclude-caches-all --ignore-failed-read
 
 echo ==== COMMANDS FOR RESTORE
-echo Restore home directory: sudo rsync -avHX --stats "$BACKUPDIR"/home /home
-echo Restore config files: sudo tar -C / -xvzf "$BACKUPDIR"/etc.tgz
+echo Restore config files: sudo tar -C / -xvzf "$BACKUPDIR"/backup.tgz <optionally specific dir e.g. home/>
 
 echo Backup complete on `date`.
-scp /tmp/backup.log "$BACKUPDIR"
